48be7dd653a51d8f2e819eaf300fb7f96a08a4bf
yuriizee <yuriizee@users.noreply.github.com>
2024-03-06 13:38:57 +0000

    Fix styling

--- src/Contracts/SensebankInstallmentContract.php
+++ src/Contracts/SensebankInstallmentContract.php
@@ -5,6 +5,8 @@ namespace StdOut\LaravelSensebankInstallment\Contracts;
 interface SensebankInstallmentContract
 {
     public function order();
+
     public function statement();
+
     public function guarantee();
 }
